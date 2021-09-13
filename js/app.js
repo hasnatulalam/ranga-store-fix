d235453199cc8c68141b70d09f150b44be6aeaa6
hasnatulalam <hasnatualam056@gmail.com>
2021-09-13 22:15:34 +0600

    final updated

--- js/app.js
+++ js/app.js
@@ -10,49 +10,78 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+         const image = product.images;
+      
+        
+     
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
+     div.classList.add("product");
+   
+
+  
+     div.innerHTML = `<div class="single-product">
+  
+        <div> 
+       
+    <img class="product-image" src=${product.image} class ="card-img-top h-75" alt="not available"></img>
+         </div>
+      <h6>${product.title}</h6>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h4>Price: $ ${product.price}</h4>
+      <p>Ratting: ${product.rating.rate} <i class="fas fa-star"></i></p>
+      <p>Review: ${product.rating.count}</p>
+     
+           
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
+      <button id="details-btn" class="btn btn-danger">Details</button>
+      </div>
+     
+  
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
-let count = 0;
+ let count = 0;
 const addToCart = (id, price) => {
-  count = count + 1;
-  updatePrice("price", price);
+    count = count + 1;
+   updatePrice("price", price);
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
-  const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+     const element = document.getElementById(id).innerText;
+      //  const converted = parseInt(element)
+      const converted = parseFloat(element)
+
   return converted;
 };
 
+
+
 // main price update function
 const updatePrice = (id, value) => {
-  const convertedOldPrice = getInputValue(id);
-  const convertPrice = parseFloat(value);
-  const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+       const convertedOldPrice = getInputValue(id);
+
+     
+      const convertPrice = parseFloat(value);
+   
+  const total = convertedOldPrice + convertPrice;  
+
+  
+  document.getElementById(id).innerText = total.toFixed(2)
+  updateTotal()
 };
 
-// set innerText function
-const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
-};
+ const setInnerText = (id, value) => {
+  
+  document.getElementById(id).innerText = value.toFixed(2)
+}; 
+const setInnerRate = (id, value) => {
+  
+  document.getElementById(id).innerText = value.toFixed(2)
+}; 
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
@@ -60,21 +89,27 @@ const updateTaxAndCharge = () => {
   if (priceConverted > 200) {
     setInnerText("delivery-charge", 30);
     setInnerText("total-tax", priceConverted * 0.2);
+     updateTotal()
   }
   if (priceConverted > 400) {
     setInnerText("delivery-charge", 50);
     setInnerText("total-tax", priceConverted * 0.3);
+    updateTotal()
   }
   if (priceConverted > 500) {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
+    updateTotal()
   }
 };
+
 
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+ 
+
+  document.getElementById("total").innerText = grandTotal.toFixed(2)
 };
